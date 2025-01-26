787897a13561d880f62048877bb37fdc7c3e4758
WuEcho <emailforecho@163.com>
2025-01-26 23:14:10 +0800

    Update Dropout.py

--- 3.MachineLearning/4.深度学习处理文本/code/Dropout.py
+++ 3.MachineLearning/4.深度学习处理文本/code/Dropout.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 import numpy as np
 
 
-"""
+""" 
 基于pytorch的网络编写
 测试dropout层
 """
@@ -13,6 +13,7 @@ import numpy as np
 import torch
 
 x = torch.Tensor([1,2,3,4,5,6,7,8,9])
+#Dropout层 每个值都有 50%的概率丢弃 有50%的概率会 乘以 1 /（1-0.5）
 dp_layer = torch.nn.Dropout(0.5)
 dp_x = dp_layer(x)
 print(dp_x)
